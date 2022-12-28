27ae9d0bc0f749d701359b292acad09facdc1839
dimonsimon2018 <dimonsimon2018@gmail.com>
2022-12-28 13:18:41 +0200

    Добавлен intersection observer

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,5 +1,23 @@
+const options = {
+   threshold : 1 
+};
+const callback = function(entries, observer){
+   entries.forEach(function(elem, index) {
+    if(elem.isIntersecting){
+ elem.target.classList.add("displayed");
+    }
+    else{
+       elem.target.classList.remove("displayed");
+    }
+	
+});
 
+    
+};
+const observer = new IntersectionObserver(callback,options);
+const targets = document.querySelectorAll('.content__snap-page');
 
+targets.forEach(p=>{observer.observe(p)});
 
 
 
