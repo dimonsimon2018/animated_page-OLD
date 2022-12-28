9321d2bb0baf2df48a97fea637e5e6e54876ca1b
dimonsimon2018 <dimonsimon2018@gmail.com>
2022-12-28 15:03:07 +0200

    Добавлено определение активного окна

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,5 @@
+var currWindow;
+
 const options = {
    threshold : 1 
 };
@@ -7,9 +9,13 @@ const callback = function(entries, observer){
  elem.target.classList.add("displayed");
     }
     else{
-       elem.target.classList.remove("displayed");
+        if(elem.target.classList.contains('displayed')){
+             elem.target.classList.remove("displayed");
+        }
+      
     }
-	
+	checkDisplayedWindow();
+    
 });
 
     
@@ -18,6 +24,20 @@ const observer = new IntersectionObserver(callback,options);
 const targets = document.querySelectorAll('.content__snap-page');
 
 targets.forEach(p=>{observer.observe(p)});
+
+
+
+function checkDisplayedWindow(){
+  targets.forEach(function(elem, index) {
+	if(elem.classList.contains('displayed')){        
+        currWindow = index;
+        // событие смены номера окна
+        console.log(currWindow);
+       
+    }
+ 
+});
+}
 
 
 
